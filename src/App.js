1cb39e6613fbc8837c61508a2c336b989d89379e
venetia <venetiafaber@gmail.com>
2024-11-18 14:47:57 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import logo from './assets/images/itranslate-logo.svg';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="itranslate logo" />
+          <h1>Welcome to iTranslate</h1>
+        </header>
+      </div>
+    </BrowserRouter>
+
   );
 }
 
