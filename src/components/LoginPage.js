fe3087c3026475826ee42a81374fa6d19e1c2dc9
Yash Patel <yashjptl2611@gmail.com>
2024-11-23 21:46:23 -0500

    Adds Icon in Input Fields

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,14 +1,21 @@
 /*
   React Component :     LoginPage.js
   Created by      :     Yash Jigneshkumar Patel
-  
 */
 
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../assets/images/itranslate-logo.svg';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faLock, faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 import Header from "./Header.js";
 
 const LoginPage = () => {
+    const [passwordVisible, setPasswordVisible] = useState(false);
+
+    const togglePasswordVisibility = () => {
+        setPasswordVisible(!passwordVisible);
+    };
+
     return (
         <>
             <Header />
@@ -25,7 +32,7 @@ const LoginPage = () => {
                             Translate between 90+ languages with confidence
                         </p>
                     </div>
-                    <ul className="nav nav-pills mb-4 justify-content-center">
+                    <ul className="nav nav-pills mb-2 justify-content-center">
                         <li className="nav-item w-50">
                             <a className="nav-link active text-center fw-semibold w-100" href="/login">Login</a>
                         </li>
@@ -33,22 +40,41 @@ const LoginPage = () => {
                             <a className="nav-link text-black-50 fw-semibold text-center w-100" href="/signup">Sign Up</a>
                         </li>
                     </ul>
-                    <form>
-                        <div className="form-group mb-3">
+                    <form className="border-top">
+                        <div className="form-group mb-3 mt-4">
                             <label className="form-label fw-semibold">Email</label>
-                            <input
-                                type="email"
-                                className="form-control"
-                                placeholder="Enter your email"
-                            />
+                            <div className="input-group">
+                                <span className="input-group-text">
+                                    <FontAwesomeIcon icon={faEnvelope} />
+                                </span>
+                                <input
+                                    type="email"
+                                    className="form-control"
+                                    placeholder="Enter your email"
+                                    required
+                                />
+                            </div>
                         </div>
                         <div className="form-group mb-3">
                             <label className="form-label fw-semibold">Password</label>
-                            <input
-                                type="password"
-                                className="form-control"
-                                placeholder="Enter your password"
-                            />
+                            <div className="input-group">
+                                <span className="input-group-text">
+                                    <FontAwesomeIcon icon={faLock} />
+                                </span>
+                                <input
+                                    type={passwordVisible ? "text" : "password"}
+                                    className="form-control"
+                                    placeholder="Enter your password"
+                                    required
+                                />
+                                <span
+                                    className="input-group-text"
+                                    style={{ cursor: "pointer" }}
+                                    onClick={togglePasswordVisibility}
+                                >
+                                    <FontAwesomeIcon icon={passwordVisible ? faEyeSlash : faEye} />
+                                </span>
+                            </div>
                         </div>
                         <div className="text-end m-3">
                             <a href="*" className="text-decoration-none">
@@ -56,7 +82,7 @@ const LoginPage = () => {
                             </a>
                         </div>
                         <button className="btn btn-primary w-100">Login</button>
-                        <div className="text-center mt-3">
+                        <div className="text-center mt-4">
                             <p className="text-muted">
                                 By continuing, you agree to iTranslate's<br />
                                 <a href="*" className="text-decoration-none">
--- src/components/SignUpPage.js
+++ src/components/SignUpPage.js
@@ -1,14 +1,26 @@
 /*
   React Component :     SigUpPage.js
   Created by      :     Yash Jigneshkumar Patel
-  
 */
 
-import React from 'react';
-import logo from '../assets/images/itranslate-logo.svg';
+import React, { useState } from "react";
+import logo from "../assets/images/itranslate-logo.svg";
 import Header from "./Header.js";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faEnvelope, faLock, faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 
 const SignupPage = () => {
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const [confirmPasswordVisible, setConfirmPasswordVisible] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible);
+  };
+
+  const toggleConfirmPasswordVisibility = () => {
+    setConfirmPasswordVisible(!confirmPasswordVisible);
+  };
+
   return (
     <>
       <Header />
@@ -25,51 +37,100 @@ const SignupPage = () => {
               Translate between 90+ languages with confidence
             </p>
           </div>
-          <ul className="nav nav-pills mb-4 justify-content-center">
+          <ul className="nav nav-pills mb-2 justify-content-center">
             <li className="nav-item w-50">
-              <a className="nav-link text-black-50 fw-semibold text-center w-100" href="/login">Login</a>
+              <a
+                className="nav-link text-black-50 fw-semibold text-center w-100"
+                href="/login"
+              >
+                Login
+              </a>
             </li>
             <li className="nav-item w-50">
-              <a className="nav-link active text-center fw-semibold w-100" href="/signup">Sign Up</a>
+              <a
+                className="nav-link active text-center fw-semibold w-100"
+                href="/signup"
+              >
+                Sign Up
+              </a>
             </li>
           </ul>
-          <form>
-            <div className="form-group mb-3">
+          <form className="border-top">
+            <div className="form-group mb-3 mt-4">
               <label className="form-label fw-semibold">Full Name</label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Enter your full name"
-              />
+              <div className="input-group">
+                <span className="input-group-text">
+                  <FontAwesomeIcon icon={faUser} />
+                </span>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Enter your full name"
+                  required
+                />
+              </div>
             </div>
             <div className="form-group mb-3">
               <label className="form-label fw-semibold">Email</label>
-              <input
-                type="email"
-                className="form-control"
-                placeholder="Enter your email"
-              />
+              <div className="input-group">
+                <span className="input-group-text">
+                  <FontAwesomeIcon icon={faEnvelope} />
+                </span>
+                <input
+                  type="email"
+                  className="form-control"
+                  placeholder="Enter your email"
+                  required
+                />
+              </div>
             </div>
             <div className="form-group mb-3">
               <label className="form-label fw-semibold">Password</label>
-              <input
-                type="password"
-                className="form-control"
-                placeholder="Create a password"
-              />
+              <div className="input-group">
+                <span className="input-group-text">
+                  <FontAwesomeIcon icon={faLock} />
+                </span>
+                <input
+                  type={passwordVisible ? "text" : "password"}
+                  className="form-control"
+                  placeholder="Create a password"
+                  required
+                />
+                <span
+                  className="input-group-text"
+                  style={{ cursor: "pointer" }}
+                  onClick={togglePasswordVisibility}
+                >
+                  <FontAwesomeIcon icon={passwordVisible ? faEyeSlash : faEye} />
+                </span>
+              </div>
             </div>
             <div className="form-group mb-4">
               <label className="form-label fw-semibold">Confirm Password</label>
-              <input
-                type="password"
-                className="form-control"
-                placeholder="Confirm your password"
-              />
+              <div className="input-group">
+                <span className="input-group-text">
+                  <FontAwesomeIcon icon={faLock} />
+                </span>
+                <input
+                  type={confirmPasswordVisible ? "text" : "password"}
+                  className="form-control"
+                  placeholder="Confirm your password"
+                  required
+                />
+                <span
+                  className="input-group-text"
+                  style={{ cursor: "pointer" }}
+                  onClick={toggleConfirmPasswordVisibility}
+                >
+                  <FontAwesomeIcon icon={confirmPasswordVisible ? faEyeSlash : faEye} />
+                </span>
+              </div>
             </div>
             <button className="btn btn-primary w-100">Sign Up</button>
-            <div className="text-center mt-3">
+            <div className="text-center mt-4">
               <p className="text-muted">
-                By continuing, you agree to iTranslate's<br />
+                By continuing, you agree to iTranslate's
+                <br />
                 <a href="*" className="text-decoration-none">
                   Terms of Service & Privacy Policy
                 </a>
